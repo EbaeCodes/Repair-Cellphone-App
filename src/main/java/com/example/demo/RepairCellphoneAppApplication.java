4de823b87acd2a89b2eba8385648af007385bfa3
Eberechukwu Nwaobiora <ebae.moon@gmail.com>
2022-03-17 22:17:30 +0100

    my second commit

--- src/main/java/com/example/demo/RepairCellphoneAppApplication.java
+++ src/main/java/com/example/demo/RepairCellphoneAppApplication.java
@@ -36,8 +36,6 @@ public class RepairCellphoneAppApplication {
 	    System.out.println("Native sql");
 	    customerRepository.SelectCustomerByFnameEqualsAndEmailEqualsNative("Ebere", "ebere@yahoo.com")
 		    .forEach(System.out::println);
-	    System.out.println("Deleting from the database");
-	    customerRepository.deleteCustomerById(1L);
 
 	};
 
